acda59d60dc16fd117e50374b61cbea206ceaca7
Siddharth <siddharth2000patel@gmail.com>
2022-12-03 18:57:54 +0530

    base code ready

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import xgboost as xgb
 import streamlit as st
 import pandas as pd
 import joblib
+import streamlit.components.v1 as components
 
 model = joblib.load("my_random_forest.joblib")
 
@@ -11,8 +11,12 @@ def predict(o, n, s, c):
     return model.predict([[o,n,s,c]])
 
 st.title('AQI Prediction')
+HtmlFile = open("./index.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code, height=300)
 st.header('Enter the values:')
-
+st.write("As current script is not crawling data automatically, I added widget to get data")
 o = st.number_input('O3')
 n = st.number_input('NO2')
 s = st.number_input('SO2')
